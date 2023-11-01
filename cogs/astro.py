a47c523c8c6d92aaf1fc2975dc2780bf50489cbb
Bugs <124417333+DezBirdss@users.noreply.github.com>
2023-11-01 17:19:23 +0000

    Add files via upload

---
+++ cogs/astro.py
@@ -0,0 +1,92 @@
+import discord
+import discord
+from discord.ext import commands
+from typing import Literal
+import datetime
+from datetime import timedelta
+import asyncio
+from discord import app_commands
+from discord.ext import commands, tasks
+import pytz
+from pymongo import MongoClient
+import platform
+import os
+from dotenv import load_dotenv
+from emojis import * 
+MONGO_URL = os.getenv('MONGO_URL')
+mongo = MongoClient(MONGO_URL)
+db = mongo['astro']
+badges = db['User Badges']
+
+
+class HelpdeskDropdown(discord.ui.Select):
+    def __init__(self):
+        options = [
+            discord.SelectOption(label='What is Astro Birb?', emoji='<:arrow:1166529434493386823>'),
+            discord.SelectOption(label="What is Quota?", emoji="<:arrow:1166529434493386823>"),
+            discord.SelectOption(label="How do I become staff here?", emoji="<:arrow:1166529434493386823>"),
+
+
+        
+            
+        ]
+        super().__init__(placeholder='Helpdesk', min_values=1, max_values=1, options=options)
+    async def callback(self, interaction: discord.Interaction):
+
+        selected = self.values[0] 
+        if selected == 'What is Astro Birb?':
+            embed = discord.Embed(title="<:Tip:1167083259444875264> What is Astro Birb?", description="> Astro Birb is a versatile bot designed for streamlining punishment and staff management tasks. It boasts a wide array of features, including but not limited to infractions, promotions, modmail, moderations, tags. What sets its infraction system apart is its comprehensive logging functionality, allowing you to effortlessly access and review the infraction history of your staff members with a simple command: /infractions {user}.")
+        elif selected == 'What is Quota?':    
+            embed = discord.Embed(title="<:Tip:1167083259444875264> What is Quota?", description="Quota is a Discord moderation bot designed to manage message quotas for staff members, ensuring their activity levels are met and helping identify potential areas for improvement or necessary punitive actions.")
+        elif selected == 'How do I become staff here?':
+            embed = discord.Embed(title="<:Tip:1167083259444875264> How do I become staff?", description="Currently support & QA is handpicked meaning you have to show your activity in the support server by either answering support forums or activily talking in the server.")
+
+        embed.color = discord.Color.dark_embed()    
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+            
+
+class Helpdesk(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.add_item(HelpdeskDropdown())
+
+class management(commands.Cog):
+    def __init__(self, client: commands.Bot):
+        self.client = client
+
+    @commands.command()
+    @commands.is_owner()    
+    async def addbadge(self, ctx, user: discord.Member, *, badge):
+        badge = {
+            'user_id': user.id,
+            'badge': badge
+        }
+        badges.insert_one(badge)  
+        await ctx.send(f"{tick} added **`{badge}`** to **@{user.display_name}**")
+
+    @commands.command()
+    @commands.is_owner()
+    async def removebadge(self, ctx, user: discord.Member, *, badge):
+        badge = {
+            'user_id': user.id,
+            'badge': badge
+        }
+        badges.delete_one(badge)  
+        await ctx.send(f"{tick} removed **`{badge}`** to **@{user.display_name}**")
+  
+    @commands.command()
+    async def helpdesk(self, ctx):
+        banner = discord.Embed(title="", color=discord.Color.dark_embed())
+        banner.set_image(url="https://cdn.discordapp.com/attachments/1104358043598200882/1169316252355936306/Astro_Banner.png?ex=6554f59f&is=6542809f&hm=5451f41931dd5e7903cdbe250ca3a521bae9a1b77fdde968721a6dead15299b5&")
+
+        main = discord.Embed(color=discord.Color.dark_embed(), title="<:Folder:1148813584957194250> Bot & Server Support", description="> Welcome to the support server for Astro Birb & Quota! These bots are useful and productive for staff management & message quotas.")
+        main.set_thumbnail(url="https://cdn.discordapp.com/icons/1092976553752789054/f9f6b1924078f9487bda623e7f6d6d71.png?size=512")
+        main.set_image(url="https://cdn.discordapp.com/attachments/1143363161609736192/1152281646414958672/invisible.png")
+        view = Helpdesk()
+        await ctx.send(embeds=(banner, main), view=view)
+
+
+
+
+async def setup(client: commands.Bot) -> None:
+    await client.add_cog(management(client))     
