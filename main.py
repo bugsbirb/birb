9fb718665b90ed28f64b7aba5fa6a8092801d694
Bugs <124417333+DezBirdss@users.noreply.github.com>
2023-11-11 13:22:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ class client(commands.Bot):
 
     
     async def on_connect(self):
-        activity2 = discord.CustomActivity(name = f"✨ {len(client.users)} users! | 🤖 V2.60")
+        activity2 = discord.CustomActivity(name = f"✨ {len(self.client.users)} users! | 🤖 V2.60")
 
         print("Connected to Discord Gateway!")
         await self.change_presence(activity=activity2)
