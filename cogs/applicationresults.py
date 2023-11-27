b9bb0683d7e40a019ecdb96d901a9d0a142019f1
Bugs <124417333+DezBirdss@users.noreply.github.com>
2023-11-27 20:16:34 +0000

    Add files via upload

--- cogs/applicationresults.py
+++ cogs/applicationresults.py
@@ -80,6 +80,16 @@ class ApplicationResults(commands.Cog):
         *,
         feedback,
     ):
+
+        if not await self.modulecheck(ctx):
+         await ctx.send(f"{no} **{ctx.author.display_name}**, this module is currently disabled.")
+         return    
+
+        if not await self.has_admin_role(ctx):
+         await ctx.send(f"{no} **{ctx.author.display_name}**, you don't have permission to use this command.")
+         return           
+
+
         if result == "Passed":
             if not feedback:
                 feedback = "None Given"
