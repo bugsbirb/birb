a5c188bd0d6ab99d8db4f4b4b431f36dac40d453
Bugsy <124417333+bugsbirb@users.noreply.github.com>
2025-12-28 14:05:11 +0000

    More readable + fingerprints

--- Cogs/Events/ConnectionRoles.py
+++ Cogs/Events/ConnectionRoles.py
@@ -4,50 +4,60 @@ from utils.emojis import *
 
 from utils.Module import ModuleCheck
 
+
 class ConnectionRolesEvent(commands.Cog):
     def __init__(self, client):
         self.client = client
 
     @commands.Cog.listener()
-    async def on_member_update(self, before, after):
-        added_roles = set(after.roles) - set(before.roles)
-        removed_roles = set(before.roles) - set(after.roles)
-        guild = after.guild
-        if not await ModuleCheck(guild.id, "connectionroles"):
+    async def on_member_update(self, before: discord.Member, after: discord.Member):
+        addRoles = set(after.roles) - set(before.roles)
+        removedRoles = set(before.roles) - set(after.roles)
+        if not await ModuleCheck(after.guild.id, "connectionroles"):
             return
-
-        for role in added_roles:
-            parent_roles_data = await self.client.db['connectionroles'].find({"parent": role.id}).to_list(
-                length=1000
+        for role in addRoles:
+            parentRoles = (
+                await self.client.db["connectionroles"]
+                .find({"parent": role.id})
+                .to_list(length=1000)
             )
-            for parent_role_data in parent_roles_data:
-                child_role_id = parent_role_data["child"]
-                child_role = after.guild.get_role(child_role_id)
-                if child_role:
+            for Parents in parentRoles:
+                childRole = after.guild.get_role(Parents.get("child"))
+                if childRole:
                     try:
-                        await after.add_roles(child_role, reason="Connection Roles updated")
+                        await after.add_roles(
+                            childRole,
+                            reason=f"Connection Roles updated. Connection Name: {Parents.get('name')}",
+                        )
                     except discord.Forbidden:
                         print("[⚠️] I don't have permission to add roles to this user")
                         return
 
-        for role in removed_roles:
-            parent_roles_data = await self.client.db['connectionroles'].find({"parent": role.id}).to_list(
-                length=1000
+        for role in removedRoles:
+            parentRoles = (
+                await self.client.db["connectionroles"]
+                .find({"parent": role.id})
+                .to_list(length=1000)
             )
-            for parent_role_data in parent_roles_data:
-                child_role_id = parent_role_data["child"]
-                child_role = after.guild.get_role(child_role_id)
-                parent_roles = await self.client.db['connectionroles'].find(
-                    {"child": child_role_id}
-                ).to_list(length=1000)
-                has_other_parent_role = any(
+            for Parents in parentRoles:
+                childId = Parents.get("child")
+                childRole = after.guild.get_role(childId)
+                parentRoles = (
+                    await self.client.db["connectionroles"]
+                    .find({"child": childId})
+                    .to_list(length=1000)
+                )
+                otherParents = any(
                     after.guild.get_role(pr["parent"]) in after.roles
-                    for pr in parent_roles
+                    for pr in parentRoles
                 )
 
-                if not has_other_parent_role and child_role:
+                if not otherParents and childRole:
                     try:
-                        await after.remove_roles(child_role, reason="Connection Roles updated")
+                        await after.remove_roles(
+                            childRole,
+                            reason=f"Connection Roles updated. Connection Name: {Parents.get('name')}",
+                        )
                     except discord.Forbidden:
                         print(
                             "[⚠️] I don't have permission to remove roles from this user"
